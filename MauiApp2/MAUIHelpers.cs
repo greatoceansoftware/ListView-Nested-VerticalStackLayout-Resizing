633661f680853cc6f3ab3316a9ef97963091bd6e
greatoceansoftware <78753788+greatoceansoftware@users.noreply.github.com>
2022-04-23 10:24:00 -0700

    Add files via upload

---
+++ MauiApp2/MAUIHelpers.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Shared;
+
+public abstract class ViewModelBase : INotifyPropertyChanged
+{
+
+  // properties
+  public static bool SuspendNotifications { get; set; }
+
+  // methods
+  internal void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+  {
+    if (SuspendNotifications == false) PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+  }
+  protected bool SetProperty<T>(ref T backingStore, T value, Action? onChanged = null, [CallerMemberName] string propertyName = "")
+  {
+
+    // no need to change if it is already set to inbound value
+    if (EqualityComparer<T>.Default.Equals(backingStore, value)) return false;
+
+    // set the backing value of the property
+    backingStore = value;
+
+    // fire the changed event
+    onChanged?.Invoke();
+    MainThread.BeginInvokeOnMainThread(() => NotifyPropertyChanged(propertyName));
+
+    // return success
+    return true;
+
+  }
+
+  // events
+  public event PropertyChangedEventHandler? PropertyChanged;
+
+}
---
+++ MauiApp2/MainPage.xaml.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections.ObjectModel;
+using System.Globalization;
+using Shared;
+
+namespace MauiApp2;
+
+public partial class MainPage : ContentPage
+{
+
+  // properties
+  public Phase Phase { get; set; } = new();
+
+  public MainPage()
+  {
+
+    // initialize
+    InitializeComponent();
+
+    // build phase
+    for (int g = 1; g <= 3; g++)
+    {
+      Goal goal = new Goal($"Goal {g}");
+      for (int a = 1; a <= 5; a++)
+      {
+        Activity activity = new Activity($"Activity {a}");
+        goal.Activities.Add(activity);
+      }
+      Phase.Goals.Add(goal);
+    }
+
+    // set context
+    BindingContext = this;
+
+  }
+
+  private void SwitchDeclutter_Toggled(object sender, ToggledEventArgs e)
+  {
+
+    Switch s = (Switch)sender;
+    foreach (var goal in Phase.Goals)
+      goal.Activities[0].IsFiltered = s.IsToggled;
+
+  }
+
+  private void ButtonReset_Clicked(object sender, EventArgs e)
+  {
+    BindingContext = null;
+    SwitchDeclutter.IsToggled = false;
+    BindingContext = this;
+  }
+
+}
+
+public class Activity : ViewModelBase
+{
+
+  // fields
+  private string name = "";
+  private string details = "";
+  private bool isFiltered;
+
+  // properties
+  public string Name { get { return name; } set { name = value; NotifyPropertyChanged(); } }
+  public string Details { get { return details; } set { details = value; NotifyPropertyChanged(); } }
+
+  public bool IsFiltered { get { return isFiltered; } set { isFiltered = value; NotifyPropertyChanged(); } }
+
+  // constructors
+  public Activity() { }
+  public Activity(string name)
+  {
+    Name = name;
+  }
+
+}
+
+public class Goal : ViewModelBase
+{
+
+  // fields
+  private ObservableCollection<Activity> activities = new();
+  private string name = "";
+
+  // properties
+  public ObservableCollection<Activity> Activities { get { return activities; } set { activities = value; NotifyPropertyChanged(); } }
+  public string Name { get { return name; } set { name = value; NotifyPropertyChanged(); } }
+
+  // constructors
+  public Goal() { }
+  public Goal(string name)
+  {
+    Name = name;
+  }
+
+}
+
+public class Phase : ViewModelBase
+{
+
+  // fields
+  private ObservableCollection<Goal> goals = new();
+  private string name = "";
+
+  // properties
+  public ObservableCollection<Goal> Goals { get { return goals; } set { goals = value; NotifyPropertyChanged(); } }
+  public string Name { get { return name; } set { name = value; NotifyPropertyChanged(); } }
+
+  // constructors
+  public Phase() { }
+  public Phase(string name)
+  {
+    Name = name;
+  }
+
+}
+
+public class BooleanTrueToVisibilityFalseConverter : IValueConverter
+{
+  public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+  {
+    return (bool?)value == true ? false : true;
+  }
+
+  public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+  {
+    throw new NotImplementedException();
+  }
+}
